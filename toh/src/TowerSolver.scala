33e0730cf42a9c9c781cfa996f4c6737da67a42e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-01-24 15:10:55 +0530

    repl method

--- toh/src/TowerSolver.scala
+++ toh/src/TowerSolver.scala
@@ -28,29 +28,51 @@ object TowerSolver {
     if (rings == 0) Vector() // For no rings, no moves needed
     else {
       val source = start.ringMap(rings) // largest peg for starting tower
-      val sink = goal.ringMap(rings)  // largest peg for goal tower
-      if (source == sink) solve(start.init, goal.init) // if largest ring is in correct place, ignore it and solve
+      val sink = goal.ringMap(rings) // largest peg for goal tower
+      if (source == sink)
+        solve(
+          start.init,
+          goal.init
+        ) // if largest ring is in correct place, ignore it and solve
       else {
-        val third = Peg.otherPeg(source, sink) // the peg not having largest ring in start or goal
-        val onThird = TowerState.simple(rings - 1, third) // set goal for all but largest ring
-        (solve(start.init, onThird) :+ // move all but largest ring to `third peg`
+        val third = Peg.otherPeg(
+          source,
+          sink
+        ) // the peg not having largest ring in start or goal
+        val onThird = TowerState.simple(
+          rings - 1,
+          third
+        ) // set goal for all but largest ring
+        (solve(
+          start.init,
+          onThird
+        ) :+ // move all but largest ring to `third peg`
           Move(source, sink)) ++ // move largest peg
           solve(
-            onThird, TowerState.simple(rings - 1, sink)
+            onThird,
+            TowerState.simple(rings - 1, sink)
           ) // move all but largest peg from third peg
       }
     }
+
   }
+
+  def standard(n: Int): Vector[Move] =
+    solve(TowerState.simple(n, Peg.Middle), TowerState.simple(n, Peg.Left))
 
   def apply(start: Peg, goal: Peg, rings: Int) =
     solve(TowerState.simple(rings, start), TowerState.simple(rings, goal))
 
   def apply(rings: Int) =
-    solve(TowerState.simple(rings, Peg.Middle), TowerState.simple(rings, Peg.Left))
+    solve(
+      TowerState.simple(rings, Peg.Middle),
+      TowerState.simple(rings, Peg.Left)
+    )
 }
 
 case class TowerState(rings: Int, ringMap: Map[Int, Peg]) {
-  def onPeg(peg: Peg): Vector[Int] = (1 to rings).toVector.filter(ringMap(_) == peg)
+  def onPeg(peg: Peg): Vector[Int] =
+    (1 to rings).toVector.filter(ringMap(_) == peg)
 
   def init = TowerState(rings - 1, ringMap filter (_._1 != rings))
 
@@ -60,13 +82,13 @@ case class TowerState(rings: Int, ringMap: Map[Int, Peg]) {
     (1 to moves.size).toVector map (moves.take(_)) map (apply)
   }
 
-  def isLegal(source: Peg, sink: Peg) : Boolean = 
+  def isLegal(source: Peg, sink: Peg): Boolean =
     source != sink &&
-    onPeg(source).headOption.map(src => 
-      onPeg(sink).headOption.map(sink => 
-        src < sink
-      ).getOrElse(true)
-      ).getOrElse(false)
+      onPeg(source).headOption
+        .map(src =>
+          onPeg(sink).headOption.map(sink => src < sink).getOrElse(true)
+        )
+        .getOrElse(false)
 }
 object TowerState {
   def simple(rings: Int, peg: Peg) =
@@ -77,11 +99,13 @@ object TowerState {
   import Peg._
 
   def random(rings: Int) =
-    TowerState(rings,
-        {(1 to rings).toVector map (
-        (j) => (j , Vector(Left, Right, Middle)((rnd.nextInt(3)))))}.toMap
+    TowerState(
+      rings, {
+        (1 to rings).toVector map ((j) =>
+          (j, Vector(Left, Right, Middle)((rnd.nextInt(3))))
+        )
+      }.toMap
     )
-
 
 }
 
